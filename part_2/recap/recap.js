50343e3179035f1463a5a102cdbef03b0c4f2fab
Vishal Jha <visual19852004@gmail.com>
2024-09-17 06:26:38 +0530

    created a todo application with adding and deleting functionality wihtout manuplating the dom everytime. It can render a new Todo everytime we click on add todo.

--- part_2/recap/recap.js
+++ part_2/recap/recap.js
@@ -1,15 +1,36 @@
-let counter = 0;
-function deleteTodo(index) {
-    let element = document.getElementById("todo-" + index)
-    element.parentNode.removeChild(element);
-}
+let counter = 1;
 
 function addTodo() {
-    let inputEl = document.getElementById("inp");
-    let newText = document.createElement("div");
-    newText.setAttribute("id", "todo-"+counter);
-    newText.style.display= "flex";
-    counter += 1;
-    newText.innerHTML="<div><h4>"+counter+". "+inputEl.value+"</h4></div><button onclick='deleteTodo("+(counter-1)+")'style='margin:20px 10px;'>delete</button>";
-    mainParent.appendChild(newText);
+    let inputEl = document.getElementById("inp").value;
+
+    let divEl = document.createElement("div");
+    let divEl2 = document.createElement("div")
+    let h4El = document.createElement("h4");
+
+    h4El.textContent = `${counter}. ${inputEl}`;
+
+    divEl.appendChild(divEl2);
+    divEl2.appendChild(h4El);
+
+    let deletebtn = document.createElement("button");
+    deletebtn.textContent = "Delete";
+    deletebtn.style.margin = "20px 10px";
+    let currentCounter = counter;
+    deletebtn.onclick = function(){
+        deleteTodo(currentCounter);
+    }
+    divEl2.appendChild(deletebtn);
+
+    document.getElementById("mainParent").appendChild(divEl);
+    divEl.setAttribute("id", "todo-"+counter);
+    divEl2.style.display= "flex";
+    counter ++;
+    
+}
+
+function deleteTodo(id) {
+    let element = document.getElementById("todo-" + id);
+    if (element) {
+        element.remove();  // Removes the entire todo element
+    }
 }
